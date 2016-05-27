dfd496cbc1500d70eb9a52ac3deb6fffda39135d
Quentin Henriet <quentin.henriet@free.fr>
2016-05-27 10:44:56 +0200

    Actions New task and New project add a widget in the tree.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QInputDialog>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -11,4 +13,47 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionNewProject_triggered()
+{
+    bool ok;
+    QString name = QInputDialog::getText(this,
+                                         tr("New project"),
+                                         tr("Enter the name of the new project:"),
+                                         QLineEdit::Normal,
+                                         QString(),
+                                         &ok);
+    if (!ok || name.isEmpty()) return;
+
+    ui->selectProject->addItem(name);
+
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    item->setText(0, name);
+    item->setText(1, "00:00:00");
+    ui->treeWidget->addTopLevelItem(item);
+}
+
+void MainWindow::on_actionNewTask_triggered()
+{
+    //We first check if a project or a task is selected
+    if (ui->treeWidget->selectedItems().empty()) return;
+
+    QTreeWidgetItem* parent = ui->treeWidget->selectedItems().first();
+
+    bool ok;
+    QString name = QInputDialog::getText(this,
+                                         tr("New sub task"),
+                                         tr("Enter the name of the new sub task:"),
+                                         QLineEdit::Normal,
+                                         QString(),
+                                         &ok);
+    if (!ok || name.isEmpty()) return;
+
+    ui->selectProject->addItem(name);
+
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    item->setText(0, name);
+    item->setText(1, "00:00:00");
+    parent->addChild(item);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionNewProject_triggered();
+
+    void on_actionNewTask_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
